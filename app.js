08dba42574e3112a1a9cbea96bc5fffd054db894
David Vega <dvega920@gmail.com>
2020-07-23 11:11:21 -0400

    required Comment model, added comment routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let bodyParser = require("body-parser"),
     Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
     express = require("express"),
     exphbs = require("express-handlebars"),
     handlebars = require("handlebars"),
@@ -100,8 +101,38 @@ app.get("/campgrounds/:id", function (req, res) {
 // *******************************************
 
 app.get("/campgrounds/:id/comments/new", function (req, res) {
-    res.render("comments/new");
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", { campground: campground });
+        }
+    })
 
+});
+
+app.post("/campgrounds/:id/comments", function (req, res) {
+    //lookup campground using ID
+    console.log(req.params);
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds")
+        } else {
+            //create new comment
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    //connect comment to new campground
+                    campground.comments.push(comment);
+                    campground.save();
+                    //redirect to campground show page
+                    res.redirect("/camprounds/" + campground._id);
+                }
+            })
+        }
+    })
 })
 
 app.listen(PORT, function () {
