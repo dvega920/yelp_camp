194ca3724b5c2e50a93975f963923c2f80cb7d83
David Vega <dvega920@gmail.com>
2020-07-24 09:39:54 -0400

    changed middleware to access 'public' directory to include '__dirname + '/public'

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.engine('handlebars', exphbs({
     handlebars: allowInsecurePrototypeAccess(handlebars)
 }));
 app.set("view engine", "handlebars");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // DATABASE CONNECTION
