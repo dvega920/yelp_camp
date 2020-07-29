f52ef7c89bbc6c7eaedb9b74f42f90988165e246
David Vega <dvega920@gmail.com>
2020-07-29 07:31:51 -0400

    created separate file for root and authentication routes. required express-router and replaced 'app' with 'router' to handle

---
+++ routes/index.js
@@ -0,0 +1,59 @@
+let express = require('express');
+let router = express.Router();
+let passport = require('passport');
+let User = require('../models/user');
+
+// Root route
+router.get("/", function (req, res) {
+    res.render("landing");
+});
+
+
+// show register form route
+router.get("/register", function (req, res) {
+    res.render("register");
+});
+
+//handle sign up logic
+router.post("/register", function (req, res) {
+    let newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.render("register")
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/campgrounds");
+        })
+    });
+});
+
+// show login form
+router.get("/login", function (req, res) {
+    res.render("login");
+});
+
+// handle login logic
+router.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function (req, res) {
+
+    });
+
+// logout route
+router.get("/logout", function (req, res) {
+    req.logOut();
+    res.redirect("/campgrounds");
+});
+
+// middleware
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login")
+}
+
+module.exports = router;
