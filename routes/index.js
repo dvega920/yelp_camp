80e2712ad1af96ee81db2f9e267a5c8943a33868
David Vega <dvega920@gmail.com>
2020-08-04 10:50:30 -0400

    refactored to use router.route()

--- routes/index.js
+++ routes/index.js
@@ -3,52 +3,49 @@ let router = express.Router();
 let passport = require('passport');
 let User = require('../models/user');
 
-// Root route
-router.get("/", function (req, res) {
-    res.render("landing");
-});
-
-
-// show register form route
-router.get("/register", function (req, res) {
-    res.render("register");
-});
-
-//handle sign up logic
-router.post("/register", function (req, res) {
-    let newUser = new User({ username: req.body.username });
-    User.register(newUser, req.body.password, function (err, user) {
-        if (err) {
-            console.log(err);
-            res.render("register")
-        }
-        passport.authenticate("local")(req, res, function () {
-            res.redirect("/campgrounds");
-        })
-    });
-});
-
-// show login form
-router.get("/login", function (req, res) {
-    res.render("login");
-});
-
-// handle login logic
-router.post("/login", passport.authenticate("local",
-    {
-        successRedirect: "/campgrounds",
-        failureRedirect: "/login"
-    }), function (req, res) {
-
+// SHOW REGISTER FORM
+router
+    .route("/register")
+    .get(function (req, res) {
+        res.render("register");
+    })
+    // HANDLES SIGNUP LOGIC
+    .post(function (req, res) {
+        let newUser = new User({ username: req.body.username });
+        User.register(newUser, req.body.password, function (err, user) {
+            if (err) {
+                console.log(err);
+                res.render("register")
+            }
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/campgrounds");
+            })
+        });
     });
 
-// logout route
-router.get("/logout", function (req, res) {
-    req.logOut();
-    res.redirect("/campgrounds");
-});
+// SHOW LOGIN FORM
+router
+    .route("/login")
+    .get(function (req, res) {
+        res.render("login");
+    })
+    // HANDLES LOGIN LOGIC
+    .post(passport.authenticate("local",
+        {
+            successRedirect: "/campgrounds",
+            failureRedirect: "/login"
+        }), function (req, res) {
+        });
 
-// middleware
+// LOGOUT ROUTE
+router
+    .route("/logout")
+    .get(function (req, res) {
+        req.logOut();
+        res.redirect("/campgrounds");
+    });
+
+//-------- MIDDLEWARE ---------//
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
