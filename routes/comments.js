b053cade2242e2b1767dac89876dff42f0474c28
David Vega <dvega920@gmail.com>
2020-08-04 11:05:35 -0400

    refactored to use router.route()

--- routes/comments.js
+++ routes/comments.js
@@ -3,49 +3,50 @@ let router = express.Router({ mergeParams: true });
 let Campground = require('../models/campground');
 let Comment = require('../models/comment');
 
-// Comments NEW
-router.get("/new", isLoggedIn, function (req, res) {
-    Campground.findById(req.params.id, function (err, campground) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("comments/new", { campground: campground });
-        }
+// COMMENTS NEW
+router.route("/new")
+    .get(isLoggedIn, function (req, res) {
+        Campground.findById(req.params.id, function (err, campground) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("comments/new", { campground: campground });
+            }
+        })
+
     })
+// COMMENTS CREATE
+router.route("/")
+    .post(isLoggedIn, function (req, res) {
+        //LOOKUP CAMPGROUND BY ID
+        console.log(req.params);
+        Campground.findById(req.params.id, function (err, campground) {
+            if (err) {
+                console.log(err);
+                res.redirect("/campgrounds")
+            } else {
+                //create new comment
+                Comment.create(req.body.comment, function (err, comment) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //add username and id to comment
+                        comment.author.id = req.user._id;
+                        comment.author.username = req.user.username;
+                        //save coment
+                        comment.save();
+                        campground.comments.push(comment);
+                        campground.save();
+                        console.log(comment);
+                        //redirect to campground show page
+                        res.redirect("/campgrounds/" + campground._id);
+                    }
+                })
+            }
+        })
+    });
 
-});
-
-// Comments CREATE
-router.post("/", isLoggedIn, function (req, res) {
-    //lookup campground using ID
-    console.log(req.params);
-    Campground.findById(req.params.id, function (err, campground) {
-        if (err) {
-            console.log(err);
-            res.redirect("/campgrounds")
-        } else {
-            //create new comment
-            Comment.create(req.body.comment, function (err, comment) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    //add username and id to comment
-                    comment.author.id = req.user._id;
-                    comment.author.username = req.user.username;
-                    //save coment
-                    comment.save();
-                    campground.comments.push(comment);
-                    campground.save();
-                    console.log(comment);
-                    //redirect to campground show page
-                    res.redirect("/campgrounds/" + campground._id);
-                }
-            })
-        }
-    })
-});
-
-// Middleware
+//-------- MIDDLEWARE ---------//
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
