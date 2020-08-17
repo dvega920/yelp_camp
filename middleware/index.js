3f9924cab3f928407ccdd17db3b6ad575a43dad4
David Vega <dvega920@gmail.com>
2020-08-17 17:55:24 -0400

    created new directory and file called index.js to store middleware code

---
+++ middleware/index.js
@@ -0,0 +1,54 @@
+let Campground = require("../models/campground");
+let Comment = require("../models/comment");
+
+let middlewareObj = {};
+
+middlewareObj.isLoggedIn = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login")
+};
+
+middlewareObj.checkCampgroundOwnership = function (req, res, next) {
+    // Checks to see if user is logged in
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, function (err, foundCampground) {
+            if (err) {
+                res.redirect("back")
+            } else {
+                // Checks if logged in user created campground
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        })
+    } else {
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCommentOwnership = function (req, res, next) {
+    // Checks to see if user is logged in
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function (err, foundComment) {
+            if (err) {
+                res.redirect("back")
+            } else {
+                // Checks if logged in user created comment
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        })
+    } else {
+        res.redirect("back");
+    }
+}
+
+module.exports = middlewareObj;
+
