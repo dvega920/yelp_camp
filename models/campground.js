ca4805352ed2b3a42ed6ede1d276728899110c28
David Vega <dvega920@gmail.com>
2020-08-24 13:29:03 -0400

    added price attribute to campground object

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ let mongoose = require('mongoose');
 // DB SCHEMA DEFINES STRUCTURE OF DB COLLECTION
 let campgroundSchema = new mongoose.Schema({
     name: String,
+    price: String,
     image: String,
     description: String,
     author: {
